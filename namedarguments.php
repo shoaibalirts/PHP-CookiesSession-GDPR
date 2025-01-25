3c6a96ecda17b7d96dfc4a930d8776bce9ae72b0
shoaibalirts <shoaibali.rts@gmail.com>
2025-01-25 12:24:44 +0100

    named arguments

---
+++ namedarguments.php
@@ -0,0 +1,8 @@
+<?php
+    function print_5x($str='Hello World', $times=5){
+        for($x=0;$x<$times;$x++){
+            echo $str.'<br />';
+        }
+    }
+    print_5x(times:3, str:'Hello Mars');
+?>
