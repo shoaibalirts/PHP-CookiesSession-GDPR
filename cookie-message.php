faa1da5c7dcb8c8e63b1e75bdf79cb70ddd7f376
shoaibalirts <shoaibali.rts@gmail.com>
2025-01-26 13:02:07 +0100

    used new session each time and new cookie value each time user sends request

--- cookie-message.php
+++ cookie-message.php
@@ -1,12 +1,14 @@
 <?php
 if(!empty($_GET['cookieok'])){
-    setcookie('cookieok','1');
+    setcookie('cookieok','1', path:'/');
     header("Location: cookie-message.php");
     die();
 }
 if (!empty($_COOKIE['cookieok'])) {
+    session_start();
+    session_regenerate_id();
     $counter = @(int) ($_COOKIE['counter'] ?? 0);
-    setcookie('counter', $counter + 1);
+    setcookie('counter', $counter + 1, path:'/');
     var_dump($counter);
 } else {
     echo "<p>Do you allow cookies: <a href='cookie-message.php?cookieok=yes'>Yes</a>, <a href='cookierules.php?cookieok=no'>No</a></p>";
